67b05e1ea96c84e6ed7559a26bc5ea05a62034fe
12329738 <n12329738@qut.edu.au>
2026-04-25 16:30:17 +1000

    Fixed

--- Assets/Scripts/BackgroundManager.cs
+++ Assets/Scripts/BackgroundManager.cs
@@ -2,35 +2,11 @@ using UnityEngine;
 
 public class BackgroundManager : MonoBehaviour
 {
-
-
     public Transform[] tiles;
     private float tileSize;
     Vector3[] originalPositions;
 
     public Transform player => GameManager.instance.player.transform;
-    void Update()
-    {
-        Vector2 playerGrid = new Vector2(
-            Mathf.Floor(player.position.x / tileSize),
-            Mathf.Floor(player.position.y / tileSize)
-        );
-
-        int gridSize = Mathf.RoundToInt(Mathf.Sqrt(tiles.Length));
-
-        for (int i = 0; i < tiles.Length; i++)
-        {
-            Vector3 basePos = originalPositions[i];
-
-            Vector3 offset = new Vector3(
-                playerGrid.x * tileSize,
-                playerGrid.y * tileSize,
-                0
-            );
-
-            tiles[i].position = basePos + offset;
-        }
-    }
 
     void Awake()
     {
@@ -40,5 +16,26 @@ public class BackgroundManager : MonoBehaviour
         originalPositions = new Vector3[tiles.Length];
         for (int i = 0; i < tiles.Length; i++)
             originalPositions[i] = tiles[i].position;
+    }
+
+    void Update()
+    {
+        Vector2 playerGrid = new Vector2(
+            Mathf.Floor(player.position.x / tileSize),
+            Mathf.Floor(player.position.z / tileSize) 
+        );
+
+        for (int i = 0; i < tiles.Length; i++)
+        {
+            Vector3 basePos = originalPositions[i];
+
+            Vector3 offset = new Vector3(
+                playerGrid.x * tileSize,
+                0,
+                playerGrid.y * tileSize 
+            );
+
+            tiles[i].position = basePos + offset;
+        }
     }
 }
